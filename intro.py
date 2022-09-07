a8095d59e4ce898041ec1ce63a8c792c5e011f9e
Susmiy <thakursusmi94@gmail.com>
2022-09-07 07:41:49 +0545

    first commit

---
+++ intro.py
@@ -0,0 +1,27 @@
+# # This is a single line comment
+
+# """
+# This is a multi line comment
+# we can write many lines here
+# as much as you want
+# no matter
+# This all above lines are consider as
+# comment in python
+# """
+
+# # this is a print function in python
+# #which provide output to the console
+# print("Hello! Batch 33B")
+
+# print(2+2)
+
+
+#name = input("Enter your name ")
+#age = int(input("Enter your age"))
+#print(f"Hi! your name is {name} and your age is {age} years old)
+
+
+#college_name = input("Enter your collegename")
+#classroom = (input("Enter your classroom"))
+#print(f"Hi! your college name is {college_name} and your classroom is {classroom} room no")
+
